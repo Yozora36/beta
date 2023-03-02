026a56cc151aaee2b56e4eed3fedf3fa17f5e5f0
Yozora36 <rikuman36@gmail.com>
2023-03-02 10:40:09 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+//var username = 'John Doe'
+//console.log(username)
+//username = 'Steve Smith'
+//console.log(username)
+
+//var greeting
+//console.log(greeting)
+//greeting = 'Hello'
+//console.log(greeting)
+
+//var $name = 'John'
+
+//var firstName = 'John' //Camel Case
+//var first_name = 'Sara' // Underscore
+//var FirstName = 'Tom' // Pascal case
+
+////let
+//let username
+//username = 'John Doe'
+//console.log(username)
+//username = 'Steve Smith'
+//console.log(username)
+
+// Const
+const name = 'John'
+console.log(name)
+// can not reassign
+
+const person = {
+  name: 'John',
+  age: 36,
+}
+
+person.name = 'Sara'
+person.age = 32
+
+//console.log(person)
+
+const number = [1, 2, 3.4, 5]
+number.push(6)
+console.log(number)
+
+// primitive data types:
+//stored directly in the locaton the variable accesses stored on the stack
+
+// reference data types:
+//accessed by reference object that are stored o n the heap a pointer to a location memory
+
+//6 primitive data type:
+//string,number,boolean,null,undefined,symbols
+
+//5 reference types of data types:
+//arrays,object literals,function,dates,anything else...
+
+// JavaScript - Dynamically typed language
